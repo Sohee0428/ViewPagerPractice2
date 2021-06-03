ecc651274c0b8c69e7e6dad01e6827020cdee2a0
Sohee <sohee3480@gmail.com>
2021-06-03 18:52:11 +0900

    어댑터 연결

--- app/src/main/java/com/example/viewpagerpractice2/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerpractice2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewpagerpractice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewpagerpractice2.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mViewPagerAdapter
+
+
     }
 }
