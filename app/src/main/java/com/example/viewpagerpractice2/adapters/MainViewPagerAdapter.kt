e16f225ea9631f057e92a59fd262811af8ae4621
Sohee <sohee3480@gmail.com>
2021-06-08 16:14:21 +0900

    페이지 별 제목 설정

--- app/src/main/java/com/example/viewpagerpractice2/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice2/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,19 @@ import com.example.viewpagerpractice2.fragments.MyInfoFragment
 import com.example.viewpagerpractice2.fragments.NameFragment
 
 class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter (fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "이름"
+        }
+        else if (position == 1) {
+            return "정보"
+        }
+        else {
+            return "인사말"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
 
         if (position == 0) {
